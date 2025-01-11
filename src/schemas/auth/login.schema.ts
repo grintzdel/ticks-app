a76ee9b143fb19dc3b3318e823be5829aed11869
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-11 20:40:40 +0100

    🔒️ Added login zod schema

---
+++ src/schemas/auth/login.schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const loginSchema = z.object({
+  email: z.string().email('Email invalide'),
+  password: z.string().min(4, 'Le mot de passe doit contenir au moins 4 caractères')
+})
+
+export type LoginData = z.infer<typeof loginSchema>

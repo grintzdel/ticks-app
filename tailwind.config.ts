b25846c90dce17d818bd750f3cdac64b97c44b2c
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-11 20:34:24 +0100

    💄 Added font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-public-sans)']
+      }
+    },
   },
   plugins: [],
 };

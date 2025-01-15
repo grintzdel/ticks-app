a890372334bea2dce39d62b4b0891c07ea8f869b
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-16 00:08:24 +0100

    :lipstick: Added Trello logo

---
+++ src/components/SVG/logos/TrelloLogo.tsx
@@ -0,0 +1,39 @@
+interface LogoProps {
+    className?: string
+}
+
+export default function TrelloLogo({className}: LogoProps) {
+    return (
+        <svg className={className}
+             xmlns="http://www.w3.org/2000/svg"
+             width="800"
+             height="800"
+             preserveAspectRatio="xMidYMid"
+             viewBox="0 0 256 256"
+        >
+            <defs>
+                <linearGradient id="linearGradient-1" x1="50%" x2="50%" y1="0%" y2="100%">
+                    <stop offset="0%" stopColor="#0091E6"></stop>
+                    <stop offset="100%" stopColor="#0079BF"></stop>
+                </linearGradient>
+            </defs>
+            <rect width="256" height="256" fill="url(#linearGradient-1)" rx="25"></rect>
+            <rect
+                width="78.08"
+                height="112"
+                x="144.64"
+                y="33.28"
+                fill="#FFF"
+                rx="12"
+            ></rect>
+            <rect
+                width="78.08"
+                height="176"
+                x="33.28"
+                y="33.28"
+                fill="#FFF"
+                rx="12"
+            ></rect>
+        </svg>
+    );
+}

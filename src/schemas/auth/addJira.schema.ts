66ca99bbf3920f4f30137ff467517e8d970b049a
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-16 20:54:46 +0100

    :necktie: Added auth Jira account link schema

--- src/schemas/auth/addJira.schema.ts
+++ src/schemas/auth/addJira.schema.ts
@@ -3,8 +3,12 @@ import {z} from 'zod'
 export const addJiraSchema = z.object({
     url: z.string()
         .min(14, 'URL invalide')
-        .transform(url => url.replace(/^https?:\/\//, '')),
-    emailjira: z.string().email('Email invalide'),
+        .transform(url => {
+            url = url.replace(/^https?:\/\//, '')
+            url = url.replace(/(.+\.net).*/, '$1')
+            return url
+        }),
+    emailJira: z.string().email('Email invalide'),
     token: z.string().min(4, 'Token invalide'),
 })
 

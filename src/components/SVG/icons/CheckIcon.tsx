a9d77a5c08ee1724b82a03c9974dc764b134d6b5
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-16 21:02:21 +0100

    :lipstick: Added check icon register form

---
+++ src/components/SVG/icons/CheckIcon.tsx
@@ -0,0 +1,19 @@
+export default function CheckIcon({className = ''}) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="9"
+            height="8"
+            fill="none"
+            viewBox="0 0 9 8"
+        >
+            <path
+                stroke="#2E68FD"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.536"
+                d="m.97 4.246 2.24 2.24L8.137 1.11"
+            ></path>
+        </svg>
+    );
+}    

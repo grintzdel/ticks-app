2b2d66357c86b6bf93cb3b499630529f086ef55a
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-16 21:02:31 +0100

    :lipstick: Added cross icon register form

---
+++ src/components/SVG/icons/CrossIcon.tsx
@@ -0,0 +1,16 @@
+export default function CrossIcon({className = ''}) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="Layer_1"
+            data-name="Layer 1"
+            viewBox="0 0 122.88 122.88"
+        >
+            <path
+                fill="#ff4141"
+                fillRule="evenodd"
+                d="M6 6a20.53 20.53 0 0 1 29 0l26.5 26.49L87.93 6a20.54 20.54 0 0 1 29 0 20.53 20.53 0 0 1 0 29L90.41 61.44l26.49 26.49a20.54 20.54 0 0 1 0 29 20.54 20.54 0 0 1-29 0L61.44 90.41 35 116.9a20.54 20.54 0 0 1-29 0 20.54 20.54 0 0 1 0-29l26.47-26.46L6 34.94A20.53 20.53 0 0 1 6 6"
+            ></path>
+        </svg>
+    );
+} 

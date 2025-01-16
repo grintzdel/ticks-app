dfd90625aacc267db7c14568e9f5e7ae04206645
grintzdel <mathis.oudin@etu.uca.fr>
2025-01-16 20:57:35 +0100

    :bug: Fixed step indicator for register auth step 2

--- src/components/ui/forms/register/PlatformCard.tsx
+++ src/components/ui/forms/register/PlatformCard.tsx
@@ -9,15 +9,19 @@ export default function PlatformCard({platformId, onLink}: PlatformCardProps) {
     const platform = platforms[platformId]
     const Logo = platform.logo
 
+    const handleClick = () => {
+        onLink(platform.nextStep)
+    }
+
     return (
         <div
-            className="flex items-center justify-between py-3 px-2 border-[#D0D5DD] rounded-lg bg-[#F9F8FF] bg-opacity-90 ">
+            className="flex items-center justify-between py-3 px-2 border-[#D0D5DD] rounded-lg bg-[#F9F8FF] bg-opacity-90">
             <div className="flex items-center gap-5">
                 <Logo className="w-14 h-14"/>
                 <span className="text-lg font-semibold">{platform.name}</span>
             </div>
             <button
-                onClick={() => onLink(platform.nextStep)}
+                onClick={handleClick}
                 className="px-6 py-2 text-white border-none rounded-full text-[15px] bg-[#3772D9] font-semibold"
             >
                 Lier

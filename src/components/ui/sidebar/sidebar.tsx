c1f854e44a8d6a13713bfa3661c309c14e60b6be
grintzdel <mathis.oudin@etu.uca.fr>
2025-02-10 11:38:04 +0100

    :truck: Moved sidebar component's files

--- src/components/ui/sidebar/sidebar.tsx
+++ src/components/ui/sidebar/sidebar.tsx
@@ -7,23 +7,23 @@ import {PanelLeftIcon} from "lucide-react"
 
 import {useIsMobile} from "@/hooks/use-mobile"
 import {cn} from "@/lib/utils"
-import {Button} from "@/components/ui/button"
-import {Input} from "@/components/ui/input"
-import {Separator} from "@/components/ui/separator"
+import {Button} from "@/components/ui/sidebar/button"
+import {Input} from "@/components/ui/sidebar/input"
+import {Separator} from "@/components/ui/sidebar/separator"
 import {
     Sheet,
     SheetContent,
     SheetDescription,
     SheetHeader,
     SheetTitle,
-} from "@/components/ui/sheet"
-import {Skeleton} from "@/components/ui/skeleton"
+} from "@/components/ui/sidebar/sheet"
+import {Skeleton} from "@/components/ui/sidebar/skeleton"
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/ui/sidebar/tooltip"
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
